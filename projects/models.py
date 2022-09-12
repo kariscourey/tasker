4bd564faa57c8ae595ca1656b01e1c4b699726c0
karis courey <kariscourey@gmail.com>
2022-09-12 17:29:20 -0400

    feat 5 complete

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ USER_MODEL = settings.AUTH_USER_MODEL
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(null=True)
     members = models.ManyToManyField(USER_MODEL, related_name="projects")
 
     def __str__(self):
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+
+def list_projects(request):
+    projects_list = Project.objects.all()
+    context = {
+        "projects_list": projects_list,
+    }
+    return render(request, "projects/list.html", context)
