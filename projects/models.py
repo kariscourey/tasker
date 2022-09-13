23d38ed27d8c37bc25b9673f78c1d96989c968dd
karis courey <kariscourey@gmail.com>
2022-09-13 01:37:19 -0400

    all tests passed

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ USER_MODEL = settings.AUTH_USER_MODEL
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True)
+    description = models.TextField(null=False)
     members = models.ManyToManyField(USER_MODEL, related_name="projects")
 
     def __str__(self):
